b57f3ccec97761ae048bf0c3a471f6325661b7fa
Kevin Altman & Ryland Herrick <pair+kaltman+rherrick@adorable.io>
2015-01-07 19:36:51 -0600

    Remove unused railties

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,12 +1,9 @@
 require File.expand_path('../boot', __FILE__)
 
 # Pick the frameworks you want:
-require "active_record/railtie"
 require "action_controller/railtie"
-require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
 
 Bundler.require(*Rails.groups)
 require "peteshow"
